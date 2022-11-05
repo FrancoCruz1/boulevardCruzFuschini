e6afb775fa22fada26aab4a5d78024221987d95b
cruz_franco <francocruz111@gmail.com>
2022-11-05 12:47:02 -0300

    Agregando boton de borrar elemento

--- productos.js
+++ productos.js
@@ -1,5 +1,5 @@
 //Variables y constantes
-const carrito = [];
+const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 let cont = document.getElementById('misProductos');
 const tablaCarro = document.getElementById('tablaCarro');
 
@@ -16,26 +16,26 @@ class Productos {
             
 }
 
-const proteina1 = new Productos ('Proteina', 'Whey Protein Hardcore x4', '../images/prote1.png', 8600, 101, 1);
-const proteina2 = new Productos ('Proteina', 'Whey Protein Gold Nutrition', '../images/prote2.png', 5200, 102, 1);
-const proteina3 = new Productos ('Proteina', 'Whey Protein 30g Scientific Body', '../images/prote3.png', 20400, 103, 1);
-const proteina4 = new Productos ('Proteina', 'Mammut Whey Protein Black', '../images/prote4.png', 21600, 104, 1);
-const proteina5 = new Productos ('Proteina', 'Whey Protein Evlution Nutrition', '../images/prote5.png',  8200, 105, 1);
-const proteina6 = new Productos ('Proteina', 'Whey Protein Gold Nutrition x3', '../images/prote6.png', 12500, 106, 1);
-const proteina7 = new Productos ('Proteina', 'ENA Whey Protein', '../images/prote7.png', 8600, 107, 1);
-const proteina8 = new Productos ('Proteina', 'ENA Whey Protein Double Rich', '../images/prote8.png', 4500, 108, 1)
-const creatina1 = new Productos ('Creatina', 'Creatina Monohidrato Gentech', '../images/creatina2.png', 9100, 109, 1);
-const creatina2 = new Productos ('Creatina', 'Creatina Monohidrato Ultra Tech', '../images/creatina3.png', 10300, 110, 1);
-const creatina3 = new Productos ('Creatina', 'Creatina Pulver Monohidratada', '../images/creatina4.png', 8100, 111, 1);
-const creatina4 = new Productos ('Creatina', 'Creatina Star Nutrition Monohidrato', '../images/creatina1.png', 10200, 112, 1);
-const vasos1 = new Productos ('Vasos mezcladores', 'Shaker Proteína Met-rx x5', '../images/shaker1.png', 9000, 113, 1);
-const vasos2 = new Productos ('Vasos mezcladores', 'Shaker Proteína KICHLY', '../images/shaker2.png', 4500, 114, 1);
-const vasos3 = new Productos ('Vasos mezcladores', 'Shaker Proteína Neon', '../images/shaker3.png', 2000, 115, 1);
-const vasos4 = new Productos ('Vasos mezcladores', 'Shaker Proteína Negro', '../images/shaker4.png', 1000, 116, 1);
-const barras1 = new Productos ('Barras protéicas', 'Barra proteica IRONBAR chocolate', '../images/barraProte1.png', 350, 117, 1);
-const barras2 = new Productos ('Barras protéicas', 'Barra proteica ENA frutilla', '../images/barraProte2.png', 350, 118, 1);
-const barras3 = new Productos ('Barras protéicas', 'Barra proteica ENA chocolate', '../images/barraProte3.png', 350, 119, 1);
-const barras4 = new Productos ('Barras protéicas', 'Barra proteica IRONBAR coco', '../images/barraProte5.png', 350, 120, 1);
+const proteina1 = new Productos ('Proteina', 'Whey Protein Hardcore x4', '../images/prote1.png', 8600, 101, 0);
+const proteina2 = new Productos ('Proteina', 'Whey Protein Gold Nutrition', '../images/prote2.png', 5200, 102, 0);
+const proteina3 = new Productos ('Proteina', 'Whey Protein 30g Scientific Body', '../images/prote3.png', 20400, 103, 0);
+const proteina4 = new Productos ('Proteina', 'Mammut Whey Protein Black', '../images/prote4.png', 21600, 104, 0);
+const proteina5 = new Productos ('Proteina', 'Whey Protein Evlution Nutrition', '../images/prote5.png',  8200, 105, 0);
+const proteina6 = new Productos ('Proteina', 'Whey Protein Gold Nutrition x3', '../images/prote6.png', 12500, 106, 0);
+const proteina7 = new Productos ('Proteina', 'ENA Whey Protein', '../images/prote7.png', 8600, 107, 0);
+const proteina8 = new Productos ('Proteina', 'ENA Whey Protein Double Rich', '../images/prote8.png', 4500, 108, 0)
+const creatina1 = new Productos ('Creatina', 'Creatina Monohidrato Gentech', '../images/creatina2.png', 9100, 109, 0);
+const creatina2 = new Productos ('Creatina', 'Creatina Monohidrato Ultra Tech', '../images/creatina3.png', 10300, 110, 0);
+const creatina3 = new Productos ('Creatina', 'Creatina Pulver Monohidratada', '../images/creatina4.png', 8100, 111, 0);
+const creatina4 = new Productos ('Creatina', 'Creatina Star Nutrition Monohidrato', '../images/creatina1.png', 10200, 112, 0);
+const vasos1 = new Productos ('Vasos mezcladores', 'Shaker Proteína Met-rx x5', '../images/shaker1.png', 9000, 113, 0);
+const vasos2 = new Productos ('Vasos mezcladores', 'Shaker Proteína KICHLY', '../images/shaker2.png', 4500, 114, 0);
+const vasos3 = new Productos ('Vasos mezcladores', 'Shaker Proteína Neon', '../images/shaker3.png', 2000, 115, 0);
+const vasos4 = new Productos ('Vasos mezcladores', 'Shaker Proteína Negro', '../images/shaker4.png', 1000, 116, 0);
+const barras1 = new Productos ('Barras protéicas', 'Barra proteica IRONBAR chocolate', '../images/barraProte1.png', 350, 117, 0);
+const barras2 = new Productos ('Barras protéicas', 'Barra proteica ENA frutilla', '../images/barraProte2.png', 350, 118, 0);
+const barras3 = new Productos ('Barras protéicas', 'Barra proteica ENA chocolate', '../images/barraProte3.png', 350, 119, 0);
+const barras4 = new Productos ('Barras protéicas', 'Barra proteica IRONBAR coco', '../images/barraProte5.png', 350, 120, 0);
 
 
 //Array de productos 
@@ -100,10 +100,12 @@ function agregarAlCarrito(prodAgregado){
     let infoTotal = document.getElementById('total');
     infoTotal.innerText='Total a pagar $: '+totalCarrito;
 }
+renderizarCarrito();
 
+//Funcion que recorre el carrito
 function renderizarCarrito(){
     tablaCarro.innerHTML = '';
-    carrito.map(prodAgregado => {
+    carrito.forEach(prodAgregado => {
         const tprod = document.createElement('tr')
         const content = `
             <tr>
@@ -111,24 +113,29 @@ function renderizarCarrito(){
                 <td>${prodAgregado.nombre}</td>
                 <td>${prodAgregado.precio}</td>
                 <td id="tdCantidad">${prodAgregado.cantidad}</td>
+                <td><button id="btnEliminar" type="button" class="btn btn-danger">X</button></td>
             </tr>
     `;
+       
         tprod.innerHTML = content;
         tablaCarro.append(tprod);
+        
+        
+        
     })
 }
 
+//Funcion para agregar al Storage
 function añadirLS(){
     localStorage.setItem('carrito', JSON.stringify(carrito))
 }
-
 const carritoGuardado = JSON.parse(localStorage.getItem('carrito'));
 if(carritoGuardado){
     carrito=carritoGuardado;
     renderizarCarrito();
 }
 
-
+//Funcion para borrar producto del carrito
 
 
 
